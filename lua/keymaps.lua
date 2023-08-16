06e219642b1031be565917c0413dbba91868039e
Artur Zakirov <zaartur@gmail.com>
2023-08-16 16:59:02 +0200

    Updated configs

---
+++ lua/keymaps.lua
@@ -0,0 +1,23 @@
+local api = vim.api
+local g = vim.g
+
+-- Remap leader and local leader to <Space>
+api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+g.mapleader = " "
+g.maplocalleader = " "
+
+-- Quickly clear search highlighting.
+vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>')
+
+-- Close all open buffers.
+vim.keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>')
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Resize with arrows.
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
---
+++ lua/options.lua
@@ -0,0 +1,30 @@
+local g = vim.g
+local opt = vim.opt
+
+-- Disable netrw for nvim-tree
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+opt.list = true
+opt.listchars:append "space:⋅"
+
+opt.termguicolors = true      -- Enable colors in terminal
+opt.hlsearch = true           -- Set highlight on search
+opt.number = true             -- Make line numbers default
+opt.breakindent = true        -- Enable break indent
+opt.undofile = true           -- Save undo history
+opt.ignorecase = true         -- Case insensitive searching unless /C or capital in search
+opt.smartcase = true          -- Smart case
+opt.updatetime = 250          -- Decrease update time
+opt.signcolumn = "yes"        -- Always show sign column
+opt.clipboard = "unnamedplus" -- Access system clipboard
+opt.timeoutlen = 300
+opt.colorcolumn = "80,120"    -- Ruler
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
