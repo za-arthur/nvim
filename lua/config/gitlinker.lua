eec4b7532cbaf0c86ba2ddd12d3257ad8d20c01d
Artur Zakirov <zaartur@gmail.com>
2024-03-01 13:11:56 +0100

    Use GitLink browse

--- lua/config/gitlinker.lua
+++ lua/config/gitlinker.lua
@@ -4,7 +4,7 @@ local M = {
   'linrongbin16/gitlinker.nvim',
   event = "BufReadPre",
   keys = {
-    utils.lazymap("<leader>gl", "<cmd>GitLink!blame<cr>", "GitLink Blame"),
+    utils.lazymap("<leader>gl", "<cmd>GitLink!browse<cr>", "GitLink Browse"),
   },
   config = function(_, opts)
     require("gitlinker").setup(opts)
