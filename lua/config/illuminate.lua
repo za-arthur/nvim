40f30d7622505a7f8d26d7700670a231436459ad
Artur Zakirov <zaartur@gmail.com>
2024-08-05 13:50:25 +0200

    Disable illuminate on large files

--- lua/config/illuminate.lua
+++ lua/config/illuminate.lua
@@ -1,6 +1,9 @@
 local M = {
   "RRethy/vim-illuminate",
   event = "VeryLazy",
+  opts = {
+    large_file_cutoff = 10000,
+  },
   config = function(_, opts)
     require('illuminate').configure(opts)
   end,
