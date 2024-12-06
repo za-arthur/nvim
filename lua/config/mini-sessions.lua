d928918a8f71cc1c3fd3369a46f60f3ff05e45cc
Artur Zakirov <zaartur@gmail.com>
2024-12-06 14:51:51 +0100

    Add "Write session" key map

--- lua/config/mini-sessions.lua
+++ lua/config/mini-sessions.lua
@@ -1,10 +1,15 @@
+local utils = require("utils")
+
 local M = {
   "echasnovski/mini.sessions",
   lazy = false,
   opts = {
     autoread = true,
     file = "Session.vim",
-  }
+  },
+  keys = {
+    utils.lazymap("<leader>S", "<cmd>lua MiniSessions.write('Session.vim')<cr>", "Write session"),
+  },
 }
 
 return M
