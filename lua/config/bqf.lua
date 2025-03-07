c0bb33e933fc4e0662a35dd2d467f8262dd46175
Artur Zakirov <zaartur@gmail.com>
2025-03-07 14:38:42 +0100

    Add keymap to bqf

--- lua/config/bqf.lua
+++ lua/config/bqf.lua
@@ -1,6 +1,12 @@
+local utils = require("utils")
+
 local M = {
   "kevinhwang91/nvim-bqf",
   event = "VeryLazy",
+  keys = {
+    utils.lazymap("]q", "<cmd>cnext<cr>", "Next quickfix"),
+    utils.lazymap("[q", "<cmd>cprevious<cr>", "Previous quickfix"),
+  },
 }
 
 return M
