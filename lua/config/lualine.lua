98c061c19f742049628f3099263f3c6b8f56fe23
Artur Zakirov <zaartur@gmail.com>
2026-02-12 19:36:04 +0100

    lua/config/lualine.lua: Update config

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -5,12 +5,14 @@ local M = {
     options = {
       icons_enabled = true,
       theme = "auto",
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
       disabled_filetypes = {},
       always_divide_middle = true,
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_b = { "branch", "diagnostics" },
       lualine_c = {
         {
           "filename",
@@ -26,9 +28,9 @@ local M = {
       lualine_a = {},
       lualine_b = {},
       lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
     },
     tabline = {},
     extensions = {},
