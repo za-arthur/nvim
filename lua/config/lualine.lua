8ad778c2c69cbc796d1b62fad04ff42408dee47c
Artur Zakirov <zaartur@gmail.com>
2026-02-13 01:52:26 +0100

    lua/config/lualine.lua: Update config

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -5,8 +5,8 @@ local M = {
     options = {
       icons_enabled = true,
       theme = "auto",
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      component_separators = { left = '', right = '|'},
+      section_separators = { left = '', right = ''},
       disabled_filetypes = {},
       always_divide_middle = true,
     },
