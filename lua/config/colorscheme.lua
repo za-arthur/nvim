6aa9c46fa57e89db9ad5148ecfd85dedab9dbe4f
Artur Zakirov <zaartur@gmail.com>
2026-03-08 22:09:07 +0100

    lua/config/colorscheme.lua: Replace tomasiser/vim-code-dark by Mofiqul/vscode.nvim

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,16 +1,9 @@
 local M = {
-  "tomasiser/vim-code-dark",
+  "Mofiqul/vscode.nvim",
   lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
+  priority = 1000,
   config = function()
-    vim.cmd("colorscheme codedark")
-    -- Fix snacks.nvim picker backgrounds
-    vim.api.nvim_set_hl(0, "SnacksPicker", { link = "Normal" })
-    -- -- Fix diff colors
-    vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#2e4b2e', fg = 'NONE' })      -- Dark green background
-    vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#45565c', fg = 'NONE' })   -- Dark yellow background
-    vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#4c1e15', fg = 'NONE' }) -- Dark red background
-    vim.api.nvim_set_hl(0, 'DiffText', { bg = '#996d74', fg = 'NONE' })     -- Highlighted changed text
+    vim.cmd("colorscheme vscode")
   end,
 }
 
