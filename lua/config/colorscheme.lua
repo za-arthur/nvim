c626ef581f692b2e977c2fcdf4d7363b6972eefe
Artur Zakirov <zaartur@gmail.com>
2026-03-09 12:58:27 +0100

    lua/config/colorscheme.lua: Decrease brightness of dimmed code

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -5,8 +5,8 @@ local M = {
   config = function()
     vim.cmd("colorscheme vscode")
     -- Adjust inactive C/C++ preprocessor blocks to be more readable
-    vim.api.nvim_set_hl(0, "@lsp.type.comment.c", { fg = "#A0A0A0" })
-    vim.api.nvim_set_hl(0, "@lsp.type.comment.cpp", { fg = "#A0A0A0" })
+    vim.api.nvim_set_hl(0, "@lsp.type.comment.c", { fg = "#858585" })
+    vim.api.nvim_set_hl(0, "@lsp.type.comment.cpp", { fg = "#858585" })
   end,
 }
 
