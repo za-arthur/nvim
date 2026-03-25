61071f2a699176e6c13f1a71762ab458a188b619
Artur Zakirov <zaartur@gmail.com>
2026-03-25 12:32:19 +0100

    lua/config/dap-view.lua: Add UI controls

--- lua/config/dap-view.lua
+++ lua/config/dap-view.lua
@@ -2,7 +2,14 @@ local utils = require("utils")
 
 local M = {
   "igorlfs/nvim-dap-view",
-  opts = {},
+  opts = {
+    winbar = {
+        controls = {
+            enabled = true,
+            buttons = { "play", "step_into", "step_over", "step_out" },
+        },
+    },
+  },
   keys = {
     utils.lazymap("<leader>dv", "<cmd>DapViewToggle<cr>", "DAP View toggle"),
     utils.lazymap("<leader>dw", "<cmd>DapViewWatch<cr>", "DAP View watch"),
